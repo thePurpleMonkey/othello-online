566e97321018207afa7c538a49199acb2976f1f7
Michael Humphrey <mhumphree@gmail.com>
2017-11-20 23:09:29 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,244 @@
+'use strict';
+
+var express     = require('express');
+var compression = require('compression');
+var socket      = require("socket.io");
+var parser      = require('body-parser');
+
+var server = express();
+
+server.use(compression());
+
+server.use(express.static("static"));
+
+server.use(parser.json());
+
+var io = socket(server.listen(8080));
+
+var waitingForMatch = null;
+
+function newGameState() {
+	// Initialize empty board
+	var board = new Array(8);
+	for (var i = 0; i < 8; i++) {
+		board[i] = new Array(8);
+		for (var j = 0; j < 8; j++) {
+			board[i][j] = ' ';
+		}
+	}
+
+	board[3][3] = 'w';
+	board[3][4] = 'b';
+	board[4][3] = 'b';
+	board[4][4] = 'w';
+
+	return board;
+}
+
+function isValidMove(state, move) {
+	if (!("x" in move && "y" in move)) {
+		state.error = "Invalid move!";
+		return false;
+
+	} else if (move.x < 0 || move.x > 7 ||
+		       move.y < 0 || move.y > 7) {
+		
+		state.error = "Invalid move!";
+		return false
+
+	} else if (state.color !== state.turn) {
+		state.error = "It's not your turn!";
+		return false;
+
+	} else if (state.board[move.x][move.y] !== ' ') {
+		state.error = "That space is already occupied!";
+		return false;
+
+	}
+
+	return true;
+}
+
+function makeMove(state, move) {
+	var directions = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]];
+	var current = {x: 0, y: 0};
+	var board = state.board;
+	var opponentColor = state.color === 'w' ? 'b' : 'w';
+
+	state.board[move.x][move.y] = state.color;
+
+	scandirection: for (var dir = 0; dir < directions.length; dir++) {
+		current.x = move.x + directions[dir][0];
+		current.y = move.y + directions[dir][1];
+		if (!(current.x < 0 || current.x > 7 || current.y < 0 || current.y > 7) && 
+		    board[current.x][current.y] !== opponentColor) {
+			continue;
+		}
+		while (!(current.x < 0 || current.x > 7 || current.y < 0 || current.y > 7) && 
+			   board[current.x][current.y] !== ' ') {
+			current.x += directions[dir][0];
+			current.y += directions[dir][1];
+
+			if (!(current.x < 0 || current.x > 7 || current.y < 0 || current.y > 7) && 
+			    board[current.x][current.y] === state.color) {
+				// We've found a potential move!
+				// Scan backwards and flip tiles
+				current.x -= directions[dir][0];
+				current.y -= directions[dir][1];
+
+				while(board[current.x][current.y] !== state.color) {
+					board[current.x][current.y] = state.color;
+					current.x -= directions[dir][0];
+					current.y -= directions[dir][1];
+				}
+
+				continue scandirection;
+			}
+		}
+	}
+
+	return state;
+}
+
+function legalMovesRemain(state) {
+	for (var i = 0; i < 8; i++) {
+		for (var j = 0; j < 8; j++) {
+			if (state.board[i][j] == ' ') {
+				if (isLegalMove(state, {x: i, y: j}, true)) {
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+function isLegalMove(state, move, silent = false) {
+	var directions = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]];
+	var current = {x: move.x, y: move.y};
+	var board = state.board;
+	var opponentColor = state.color === 'w' ? 'b' : 'w';
+
+	for (var dir = 0; dir < directions.length; dir++) {
+		current.x = move.x + directions[dir][0];
+		current.y = move.y + directions[dir][1];
+		if (!(current.x < 0 || current.x > 7 || current.y < 0 || current.y > 7) && 
+		    board[current.x][current.y] !== opponentColor) {
+			continue;
+		}
+
+		while (!(current.x < 0 || current.x > 7 || current.y < 0 || current.y > 7) && 
+		       board[current.x][current.y] !== ' ') {
+			current.x += directions[dir][0];
+			current.y += directions[dir][1];
+
+			if (!(current.x < 0 || current.x > 7 || current.y < 0 || current.y > 7) && 
+			    board[current.x][current.y] === state.color) {
+				return directions[dir];
+				//return true;
+			}
+		}
+	}
+	
+	if (!silent) {
+		state.error = "That is not a legal move.";
+	}
+	return false;
+}
+
+io.on("connection", function(clientSocket) {
+	console.log("New connection");
+
+	clientSocket.on("waitingForOpponent", function(multiplayer) {
+		if (multiplayer) {
+			// Multiplayer
+			if (!waitingForMatch) {
+				// No match available
+				waitingForMatch = clientSocket;
+				console.log("Added client to wait queue");
+			} else {
+				console.log("Opponents matched!");
+
+				// Make match
+				clientSocket.opponent = waitingForMatch;
+				var otherSocket = waitingForMatch;
+				waitingForMatch = null;
+				otherSocket.opponent = clientSocket;
+
+				var state = {
+					board: newGameState(),
+					color: 'w',
+					turn:  'b',
+					multiplayer: true
+				};
+				clientSocket.state = state;
+				clientSocket.emit("newState", state);
+
+				state = {
+					board: state.board,
+					color: 'b',
+					turn: 'b',
+					multiplayer: true
+				};
+				otherSocket.state = state
+				otherSocket.emit("newState", state);
+			}
+		} else {
+			// Single player
+			var state = {
+				board: newGameState(),
+				color: ['b', 'w'][Math.round(Math.random())],
+				turn:  'b',
+				multiplayer: false
+			};
+			clientSocket.state = state;
+			clientSocket.emit("newState", state);
+		}
+	});
+
+	clientSocket.on("move", function(move) {
+		if (isValidMove(clientSocket.state, move) && isLegalMove(clientSocket.state, move)) {
+			// Legal move
+			//clientSocket.state.board[move.x][move.y] = clientSocket.state.color;
+			makeMove(clientSocket.state, move);
+			clientSocket.state.turn = clientSocket.state.turn === 'w' ? 'b' : 'w';
+			clientSocket.state.error = null;
+
+			clientSocket.opponent.state.board = clientSocket.state.board;
+			clientSocket.opponent.state.turn = clientSocket.state.turn;
+			clientSocket.opponent.state.error = null;
+
+			if (legalMovesRemain(clientSocket.opponent.state)) {
+				// Opponent has a move, play can continue normally
+				clientSocket.emit("newState", clientSocket.state);
+				clientSocket.opponent.emit("newState", clientSocket.opponent.state);
+			} else if (legalMovesRemain(clientSocket.state)) {
+				// Opponent cannot move, skip their turn
+			} else {
+				// Nobody can move, game over.
+			}
+
+		} else {
+			clientSocket.emit("newState", clientSocket.state);
+		}
+	});
+
+	clientSocket.on("disconnect", function() {
+		if (waitingForMatch === clientSocket) {
+			waitingForMatch = null;
+			console.log("Removed client from wait queue");
+		}
+
+		if (clientSocket.opponent) {
+			clientSocket.opponent.emit("clientDisconnected");
+		}
+	});
+});
+
+server.get('/ai', function(req, res) {
+	var state = req.body;
+
+	res.status(501);
+	res.end();
+});
---
+++ static/main.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var socket = io.connect('/');
+
+var state;
+var highlighted = null;
+
+$(".big-button").bind("click", function(event) {
+	var multiplayer = this.id === "multiplayer-button";
+	console.log("Multiplayer: " + multiplayer);
+	$("#landing").addClass("hidden");
+	$("#finding-opponent").removeClass("hidden");
+	socket.emit("waitingForOpponent", multiplayer);
+});
+
+socket.on("connect", function() {
+	console.log("Connected!");
+	$("body > div").addClass("hidden");
+	$("#landing").removeClass("hidden");
+})
+
+socket.on("disconnect", function() {
+	$("body > div").addClass("hidden");
+	$("#error").removeClass("hidden");
+	console.log("Disconnected!");
+});
+
+socket.on("newState", function(newState) {
+	$("#finding-opponent").addClass("hidden");
+	$("#game-div").removeClass("hidden");
+	state = newState;
+
+	if (state.error) {
+		$("#errorMsg").text(state.error);
+		$("#errorMsg").removeClass("hidden");
+	} else {
+		$("#errorMsg").addClass("hidden");
+	}
+
+	if (state.color === state.turn) {
+		$("#turn").removeClass("hidden");
+		$("#waitingForOpponent").addClass("hidden");
+	} else {
+		$("#turn").addClass("hidden");
+		$("#waitingForOpponent").removeClass("hidden");
+	}
+
+	if (state.color === 'w') {
+		$("#player").text("light");
+	} else {
+		$("#player").text("dark");
+	}
+
+	var blackScore = 0;
+	var whiteScore = 0;
+	for (var i = 0; i < 8; i++) {
+		for (var j = 0; j < 8; j++) {
+			if (state.board[i][j] === 'w') {
+				whiteScore++;
+			} else if (state.board[i][j] === 'b') {
+				blackScore++;
+			}
+		}
+	}
+
+	$("#white-score").text(whiteScore);
+	$("#black-score").text(blackScore);
+
+	gameLoop();
+});
+
+socket.on("clientDisconnected", function() {
+	console.log("Client disconnected");
+	socket.disconnect();
+	socket.open();
+});
+
+var context = $("#game-canvas").get(0).getContext("2d");
+var imageSrcs = ["reversi_board.png", "white.png", "black.png", "cursor.png"];
+var images = [];
+
+function displayBoard() {
+	context.drawImage(images[0], 0, 0);
+	for (var x = 0; x < 8; x++) {
+		for (var y = 0; y < 8; y++) {
+			switch (state.board[x][y]) {
+				case 'b':
+					context.drawImage(images[2], (x*64)+(4*(x+1)), (y*64)+(4*(y+1)));
+					break;
+					
+				case 'w':
+					context.drawImage(images[1], (x*64)+(4*(x+1)), (y*64)+(4*(y+1)));
+					break;
+			}
+		}
+	}
+
+	if (highlighted && state.turn === state.color) {
+		context.drawImage(images[3], ((highlighted.x*64)+(4*(highlighted.x+1)))-1, 
+		                             ((highlighted.y*64)+(4*(highlighted.y+1)))-1);
+	}
+}
+
+for (var i=0; i<imageSrcs.length; i++) {
+	var image = new Image();
+	image.src = imageSrcs[i];
+	images.push(image);
+}
+
+function gameLoop() {
+	
+	//Loop this function at 60 frames per second
+	requestAnimationFrame(gameLoop);
+
+	//Render the stage to see the animation
+	if (state) {
+		displayBoard(state.board);
+	}
+}
+
+$("#game-canvas").on("mousemove", function(event) {
+	var squareX = Math.floor((event.pageX - $(this).offset().left) / 68);
+	var squareY = Math.floor((event.pageY - $(this).offset().top) / 68);
+	//console.log(squareX + ", " + squareY);
+	highlighted = {x: squareX, y: squareY};
+});
+
+$("#game-canvas").on("mouseleave", function(event) {
+	highlighted = null;
+});
+
+$("#game-canvas").on("click", function(event) {
+	var squareX = Math.floor((event.pageX - $(this).offset().left) / 68);
+	var squareY = Math.floor((event.pageY - $(this).offset().top) / 68);
+	state.board[squareX][squareY] = state.color;
+	state.turn = null; // Just make sure the user doesn't screw anything by clicking
+
+	if (state.multiplayer) {
+		// Multiplayer
+		
+		console.log("Sending move (" + squareX + ", " + squareY + ")");
+		socket.emit("move", {x: squareX, y: squareY});
+	} else {
+		// Single player.
+		// Make an AJAX request for the computer's move
+	}
+});
